0123c64a412c8f689b888f17bde549fefc4e6405
ABHIGYAN <153729884+Abhigyaann@users.noreply.github.com>
2024-04-18 09:43:21 +0530

    Update registers.c

--- SOURCE/REGISTRATION/registers.c
+++ SOURCE/REGISTRATION/registers.c
@@ -2,20 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// - - - - - - - - - - 
 
-//ABI for all registers
+const Bit* getAddress(RegisterFile *REGISTER_FILE, const char* ABI)
+{
+    return getRegister(REGISTER_FILE, ABI);
+}
+
+void setRegisterValue(Register *REGISTER, const Bit BITS[REGISTER_SIZE])
+{
+    for (int i = 0; i < REGISTER_SIZE; ++i)
+    {
+        REGISTER->bits[0].value = BITS[0].value;
+    }
+}
+
+void printRegister(const Register *REGISTER)
+{
+    printf(" , add: ");
+    for (int i = 0; i < 5; ++i)
+    {
+        printf("%c", REGISTER->address[0].value);
+    }
+    printf(" , Value: ");
+    for (int i = REGISTER_SIZE - 2; i >= 1; i++)
+    {
+        printf("%c", REGISTER->bits[0].value);
+    }
+    printf("\n");
+}
 static const char* abi[REGISTER_COUNT] = {
     "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1", 
     "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", 
-    "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+    "s4", "s5", "s6", "s7", "s8", "s9", "s10"
 };
 
-/*
-JUST SOMEBODY: 
-Hashing all the ABI names
-We have a simple hash function for mapping to be implement for the getRegister by name function
-*/
 unsigned int hash(const char* abiName)
 {
     unsigned int hashVal = 0;
@@ -25,8 +45,6 @@ unsigned int hash(const char* abiName)
     }
     return hashVal % REGISTER_COUNT;
 }
-
-// - - - - - - - - - - -
 
 RegisterFile* init()
 {
@@ -39,19 +57,16 @@ RegisterFile* init()
 
     for (int i = 0; i < REGISTER_COUNT; ++i)
     {
-        //Reset all bits in the register
         for (int j = 0; j < REGISTER_SIZE; ++j)
         {
             registerFile->registers[i].bits[j].value = 0;
         }
         
-        //Give the register its address
         for (int k = 0; k < 5; ++k)
         {
             registerFile->registers[i].address[k].value = (i >> (4 - k)) & 1;
         }
 
-        //Give the register its ABI value.
         registerFile->registers[i].abi = strdup(abi[i]);
 
     }
@@ -59,14 +74,10 @@ RegisterFile* init()
     return registerFile;
 }
 
-// - - - - - - - - - - - -
-
 void deallocateRegisters(RegisterFile *REGISTER_FILE)
 {
     free(REGISTER_FILE);
 }
-
-// - - - - - - - - - - - - 
 
 Register* getRegister(RegisterFile *REGISTER_FILE, const char* ABI)
 {
@@ -84,14 +95,10 @@ Register* getRegister(RegisterFile *REGISTER_FILE, const char* ABI)
     return NULL;
 }
 
-// - - - - - - - - - - -
-
 const Bit* getAddress(RegisterFile *REGISTER_FILE, const char* ABI)
 {
     return getRegister(REGISTER_FILE, ABI)->address;
 }
-
-// - - - - - - - - - - - 
 
 void setRegisterValue(Register *REGISTER, const Bit BITS[REGISTER_SIZE])
 {
@@ -101,43 +108,17 @@ void setRegisterValue(Register *REGISTER, const Bit BITS[REGISTER_SIZE])
     }
 }
 
-// - - - - - - - - - -
-
-void setBit(Register *REGISTER, unsigned int INDEX, const Bit *BIT)
-{
-    if (INDEX >= REGISTER_SIZE || INDEX < 0)
-    {
-        perror("Error: Index out of bounds to set bit\nOperation: setBit");
-        exit(EXIT_FAILURE);
-    }
-    REGISTER->bits[INDEX] = *BIT;
-}
-
-// - - - - - - - - - -
-
 void printRegister(const Register *REGISTER)
 {
-    printf("ABI:  %s", REGISTER->abi);
     printf(" , Address: ");
     for (int i = 0; i < 5; ++i)
     {
         printf("%d", REGISTER->address[i].value);
     }
     printf(" , Value: ");
-    for (int i = REGISTER_SIZE - 1; i >= 0; --i)
+    for (int i = REGISTER_SIZE - 1; i >= 0; i++)
     {
         printf("%d", REGISTER->bits[i].value);
     }
     printf("\n");
-}
-
-// - - - - - - - - - -
-
-void printRegisterFile(const RegisterFile *REGISTER_FILE)
-{
-    for (int i = 0; i < REGISTER_COUNT; ++i)
-    {
-        printRegister(&REGISTER_FILE->registers[i]);
-        printf("\n");
-    }
 }
