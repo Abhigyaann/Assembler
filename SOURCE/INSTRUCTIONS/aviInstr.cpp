20a2688722772bd182f508cfe70d988a7cdd1d9a
ABHIGYAN <153729884+Abhigyaann@users.noreply.github.com>
2024-04-18 09:41:50 +0530

    Update aviInstr.cpp

--- SOURCE/INSTRUCTIONS/aviInstr.cpp
+++ SOURCE/INSTRUCTIONS/aviInstr.cpp
@@ -1,26 +1,12 @@
-#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 
 using namespace std;
-
-// trim from end of string (right)
 inline std::string &rtrim(std::string &s, const char *t = " \t\n\r\f\v") {
   s.erase(s.find_last_not_of(t) + 1);
   return s;
-}
-
-// trim from beginning of string (left)
-inline std::string &ltrim(std::string &s, const char *t = " \t\n\r\f\v") {
-  s.erase(0, s.find_first_not_of(t));
-  return s;
-}
-
-// trim from both ends of string (right then left)
-inline std::string &trim(std::string &s, const char *t = " \t\n\r\f\v") {
-  return ltrim(rtrim(s, t), t);
 }
 
 void tokenizer(string line, string tokenArray[], char del) {
@@ -66,18 +52,9 @@ int main() {
   //     cout << tokens[i] << endl;
   //   }
   // }
-  const string filename = "src/instruction/text.txt";
+
 
   int numLines;
   string *lines = readTextFile(filename, numLines);
   tokenizer(lines[0], tokens, ' ');
-  tokenizer(tokens[1], tokens, ',');
-
-  for (int i = 0; i < 5; i++) {
-    if (tokens[i] != "") {
-      cout << tokens[i] << endl;
-    }
-  }
-  delete[] lines;
-  return 0;
-}
+  tokenizer(tokens[1], tokens, ',');}
